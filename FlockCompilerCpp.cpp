e4fe27a384687fc5a2dae1b25cd2ba76f87a827d
jackmoxley <jackokmoxley@gmail.com>
2020-09-05 19:00:41 +0100

    Added Apache 2 License Headers

--- FlockCompilerCpp.cpp
+++ FlockCompilerCpp.cpp
@@ -1,6 +1,20 @@
-// FlockCompilerCpp.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+/*
+ * Copyright 2020 John Orlando Keleshian Moxley
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
+// FlockCompilerCpp.cpp : This file contains the 'main' function. Program execution begins and ends there.
 // Fixes for specific compilers and IDEs go in here, try not to polute the rest of the code.
 #include "CompilerFix.h"
 
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
+[comment]: # (Copyright 2020 John Orlando Keleshian Moxley)
+[comment]: # (Licensed under the Apache License, Version 2.0 (the "License");)
+[comment]: # (you may not use this file except in compliance with the License.)
+[comment]: # (You may obtain a copy of the License at)
+
+[comment]: # ( http://www.apache.org/licenses/LICENSE-2.0)
+
+[comment]: # (Unless required by applicable law or agreed to in writing, software)
+[comment]: # (distributed under the License is distributed on an "AS IS" BASIS,)
+[comment]: # (WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.)
+[comment]: # (See the License for the specific language governing permissions and)
+[comment]: # (limitations under the License.)
 # flock-compiler-cpp
+ 
